7671b42cc7fc98d04950a13e58d6c2afb85fcefe
filippo <tosifilippo@outlook.it>
2021-02-01 19:57:11 +0100

    changed buttons to images, js takes img alt to determine player choice, added functions to keep and return score, added function to declare winner

--- script.js
+++ script.js
@@ -1,58 +1,81 @@
 let playerScore=0
 let computerScore=0
+let computerSelection 
+let playerSelection;
+
+let buttons = document.querySelectorAll(".button");
+const endDesc = document.querySelector("#end-desc");
+const container = document.querySelector("#results-container");
+
+buttons.forEach((button) => {
+	button.addEventListener("click", () => {
+		const img = button.querySelector("img");
+		playerSelection = img.alt;
+
+		playRound(playerSelection, computerSelection);
+		if (playerScore === 5 || computerScore === 5) {
+      declareWinner();
+    }
+		
+	});
+});
+
+const myArray = ["Sasso", "Carta", "Forbici"];
 
 function computerPlay() {
-	let randomValue = Math.floor(Math.random() * 3);
-	if (randomValue === 0) {
-			return "carta"
-	} else if (randomValue === 1) {
-			return "sasso"
+  return myArray[~~(Math.random() * myArray.length)];
+}
+
+function playRound(playerSelection, computerSelection) {
+
+	computerSelection = computerPlay().toLowerCase();
+	playerSelection = playerSelection.toLowerCase();
+		
+	if (playerSelection == computerSelection) {
+			displayResults("Pari!")
+	} else if (
+    (computerSelection == "sasso" && playerSelection == "forbici") ||
+    (computerSelection == "forbici" && playerSelection == "carta") ||
+    (computerSelection == "carta" && playerSelection == "sasso")
+  ) {
+    computerScore = ++computerScore;
+		keepCpuScore();
+		displayResults(
+			`Oh no! Hai perso.
+			${capitalize(computerSelection)} batte ${playerSelection}.`
+		);
 	} else {
-			return "forbici"
+    playerScore = ++playerScore;
+    keepPlayerScore();
+		displayResults(
+			`Evvai!!! Hai vinto.
+			${capitalize(playerSelection)} batte ${computerSelection}.`
+		);
 	}
 }
 
-function game() {
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
-	for (let y=1; y < 6; y++) {
-	const playerSelection = document.getElementsByClassName("buttons").value;
-	const computerSelection = computerPlay();
-	console.log(playRound(playerSelection, computerSelection));
-	console.log(playerScore);
-	console.log(computerScore);
-	}
+function displayResults(str) {
+  container.textContent = str;
+}
 
+function declareWinner() {
 	if (playerScore > computerScore) {
-			return "Hai vinto!" 
-	} else if (playerScore < computerScore) {
-			return "Hai perso!"
+	endDesc.textContent = "Hai vinto!";
 	} else {
-			return "Pari!"
-	}
-	
-	function playRound(playerSelection, computerSelection) {
-		if (playerSelection == computerSelection) {
-				// return "Pari! Prova ancora."
-		} else if ((playerSelection == "sasso") && (computerSelection == "forbici")) {
-				// return "Hai vinto! Sasso batte forbici."
-				playerScore = ++playerScore
-		} else if ((playerSelection == "sasso") && (computerSelection == "carta")) {
-				// return "Hai perso! Carta batte sasso."
-				computerScore = ++computerScore
-		} else if ((playerSelection == "forbici") && (computerSelection == "carta")) {
-				// return "Hai vinto! Forbici batte carta."
-				playerScore = ++playerScore
-		} else if ((playerSelection == "forbici") && (computerSelection == "sasso")) {
-				// return "Hai perso! Sasso batte forbici."
-				computerScore = ++computerScore
-		} else if ((playerSelection == "carta") && (computerSelection == "sasso")) {
-				// return "Hai vinto! Carta batte sasso."
-				playerScore = ++playerScore
-		} else if ((playerSelection == "carta") && (computerSelection == "forbici")) {
-				// return "Hai perso! Forbice batte carta."
-				computerScore = ++computerScore
-		} else {
-				// return "Giocata non valida! Non barare."
-		}
-	}
+	endDesc.textContent = "Hai perso...";
+ }
+ }
+
+function keepPlayerScore() {
+  let playerScoreBox = document.querySelector("#player-score");
+  playerScoreBox.textContent = playerScore;
+}
+
+function keepCpuScore() {
+	let computerScoreBox = document.querySelector("#computer-score");
+	computerScoreBox.textContent = computerScore;
 }
